3cf4383bf89deb435af618020209a785bcf444b6
svizcaino26 <svizcaino26@gmail.com>
2026-04-06 18:13:04 -0500

    implement Product model and database action to add new products to the database

---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use sqlx::{PgPool};
+use thiserror::Error;
+use std::fmt::Display;
+use time::PrimitiveDateTime;
+use typesafe_builder::*;
+
+#[derive(FromRow, Debug, Serialize, Deserialize)]
+pub struct Product {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    price_cents: i32,
+    created_at: PrimitiveDateTime,
+    updated_at: PrimitiveDateTime,
+}
+
+impl Display for Product {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Product: {} - {}\nPrice: {}\n",
+            self.name, self.id, self.price_cents
+        )
+    }
+}
+
+#[derive(Builder, Debug, Serialize, Deserialize)]
+pub struct NewProduct {
+    #[builder(required, into)]
+    name: String,
+    #[builder(required)]
+    price_cents: i32,
+    #[builder(optional, into)]
+    description: Option<String>,
+}
+
+impl NewProduct {
+    pub fn validate(self) -> Result<Self, AppError> {
+        if self.price_cents < 0 {
+            return Err(ValidationError::InvalidPrice(self.price_cents).into());
+        }
+        Ok(self)
+    }
+}
+
+pub async fn create_product(
+    pool: &PgPool,
+    new_product: NewProduct
+) -> Result<Product, AppError> {
+    let new_product = new_product.validate()?;
+    let product = sqlx::query_as!(
+        Product,
+        r#"
+            INSERT INTO products (name, price_cents, description)
+            VALUES ($1, $2, $3)
+            RETURNING *
+        "#,
+        new_product.name,
+        new_product.price_cents,
+        new_product.description
+    ).fetch_one(pool).await?;
+    Ok(product)
+}
+
+#[derive(Debug, Error)]
+pub enum ValidationError {
+    #[error("Invalid price {0}")]
+    InvalidPrice(i32),
+}
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("{0}")]
+    Database(#[from] sqlx::Error),
+    #[error("{0}")]
+    Validation(#[from] ValidationError)
+}
