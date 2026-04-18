e8617a60c425cdef9b43e8d9e3fbe01ebb880d3c
svizcaino26 <svizcaino26@gmail.com>
2026-04-18 18:57:00 -0500

    add and test pstgresql connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use oms_rust::{NewProductBuilder, create_product};
+use std::env;
+use sqlx::PgPool;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().unwrap();
+
+    let db = PgPool::connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
+    let new_product = NewProductBuilder::new()
+        .with_name("big monitor")
+        .with_price_cents(9900)
+        .with_description("Big monitor")
+        .build();
+
+    let product = create_product(&db, new_product).await?;
+
+    println!("{product:?}");
+    Ok(())
 }
